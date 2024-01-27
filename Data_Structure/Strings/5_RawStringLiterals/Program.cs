f6e27406f27587566c522fcb26dd9349fe1d0be4
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-01-27 16:04:06 +0530

    String interpolation

---
+++ Data_Structure/Strings/5_RawStringLiterals/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+/*
+     Refer this link for know more about raw string literals...
+    https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/strings/
+*/
+
+
+namespace pr5
+{
+    class q5
+    {
+        static void Main(string[] args)
+        {
+            string singleLine = """Friends say "hello" as they pass by.""";
+            Console.WriteLine($"SingleLine: {singleLine}");
+
+
+            string multiLine = """
+                "Hello World!" is typically the first program someone writes.
+                """;
+            Console.WriteLine($"\nMultiLine: {multiLine}");
+
+
+            string embeddedXML = """
+                <element attr = "content">
+                    <body style="normal">
+                        Here is the main text.
+                    </body>
+                    <footer>
+                        Excerpts from "An amazing story"
+                    </footer>
+                </element>
+                """;
+            Console.WriteLine($"\nEmbeddedXML: \n{embeddedXML}");
+
+
+
+            // The line "<element attr = "content">" starts in the first column.
+            // All whitespace left of that column is removed from the string.
+
+            string raw_String_Literal_Delimiter = """"
+                Raw string literals are delimited
+                by a string of at least three double quotes,
+                like this:  """
+                """";
+            Console.WriteLine($"\nRaw String Literals: {raw_String_Literal_Delimiter}\n");
+
+        }
+    }
+}
---
+++ Data_Structure/Strings/6_RawStringLiterals/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace pr6
+{
+    class q6
+    {
+        static void Main(string[] args)
+        {
+            // CS8999: Line does not start with the same whitespace as the closing line
+            // of the raw string literal
+            Console.WriteLine("__Example 1__");
+            var noOutdenting = """
+                        A line of text.
+                    Trying to outdent the second line.
+                """;
+            Console.WriteLine($"No outdenting: {noOutdenting}\n");
+
+//------------------------------------------------------------------------------------------------------------
+
+            Console.WriteLine("\n__Example 2__");
+            string jsonString = """ 
+            {
+                "Date": "2024-Jan-27; Time: 10:00-16:00",
+                "TemperatureCelsius": 25,
+                "Summary": "Normal",
+                "DatesAvailable": [
+                    "2024-01-27T10:00-16:00",
+                    "2024-01-28T10:00-16:00"
+                ],
+                "TemperatureRanges": [
+                    "Cold": {
+                        "High": 20,
+                        "Low": -10
+                    },
+                    "Normal": {
+                        "High": 28,
+                        "Low": 21
+                    },
+                    "Hot": {
+                        "High": 60
+                        "Low": 29
+                    }
+                   ],
+                "SummaryWords": [
+                    "Cool",
+                    "Windy",
+                    "Humid"
+                   ]
+            }
+            """;
+            Console.WriteLine($"Json String: \n{jsonString}");
+
+
+            //----------------------------------------------------------------------------
+
+
+            Console.WriteLine("\n__Example 3__");
+            var jh = (firstName: "Jupiter", lastName: "Hammon", born: 1711, published: 1761);
+            Console.WriteLine($"{jh.firstName} {jh.lastName} was an African American poet born in {jh.born}.");
+            Console.WriteLine($"He was first published in {jh.published} at the age of {jh.published - jh.born}.");
+            Console.WriteLine($"He'd be over {Math.Round((2018d - jh.born) / 100d) * 100d} years old today.");
+
+            // Output:
+            // Jupiter Hammon was an African American poet born in 1711.
+            // He was first published in 1761 at the age of 50.
+            // He'd be over 300 years old today.
+
+
+            //----------------------------------------------------------------------------------------------------------------------
+
+
+            Console.WriteLine("\n__Example 4__");
+            var infoMsg = (Brand: "HP", Series: "Omen", ModelNo: 16, ModelName: "Transcend", ReleaseMonth: "June", ReleaseYear: 2023);
+            Console.WriteLine($"Brand new {infoMsg.Brand} release its new {infoMsg.Series} series model: {infoMsg.ModelName}{infoMsg.ModelNo}");
+            Console.WriteLine($"The model is released on {infoMsg.ReleaseMonth},{infoMsg.ReleaseYear}\n");
+        }
+    }
+}
