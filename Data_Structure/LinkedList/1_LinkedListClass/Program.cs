bc05e26e3d4641666f7ac05fa12ebee8b575c966
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-01-27 12:16:17 +0530

    Updated files

---
+++ Data_Structure/LinkedList/1_LinkedListClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace pr1
+{
+    class q1
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<String> myList = new LinkedList<string>();
+            myList.AddLast("Programmers");
+            myList.AddLast("Welcome");
+            myList.AddLast("to C#");
+            myList.AddLast("Programming Language");
+            Console.WriteLine(myList.ToString());
+            if (myList.Count > 0) {
+                Console.WriteLine("LinkedList is not empty.");
+            } else {
+                Console.WriteLine("LinkedList is empty.");
+            }
+        }
+    }
+}
