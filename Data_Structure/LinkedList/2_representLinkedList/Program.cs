0ec014a95ca313c3c18b2c806883178c570ff39c
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-01-29 17:36:13 +0530

    updated

---
+++ Data_Structure/LinkedList/2_representLinkedList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace pr2
+{
+    public class Node {
+        private static int data;
+        private static Node next;
+    }
+
+    public class LinkedListCreate {
+        public Node head;
+    }
+}
