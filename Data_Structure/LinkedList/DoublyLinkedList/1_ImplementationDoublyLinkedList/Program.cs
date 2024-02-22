ade3531608cf51bfc9416126c59a2d45d290a34c
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-02-22 16:16:59 +0530

    updated doubly linked list folder

---
+++ Data_Structure/LinkedList/DoublyLinkedList/1_ImplementationDoublyLinkedList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DoubleLinkedList {
+
+    public class ListNode {
+        public int data;
+        public ListNode? next;
+        public ListNode? prev;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+
+    class DoublyLinkedList {
+        private static ListNode? head = null;
+        private static ListNode? tail = null;
+        // main method
+        static void Main(string[] args) {
+            head = new ListNode(10);
+            ListNode second = new ListNode(20);
+            ListNode third = new ListNode(30);
+
+            head.next = second;
+            second.prev = head;
+            second.next = third;
+            third.prev = second;
+            tail = third;
+        }
+    }
+}
