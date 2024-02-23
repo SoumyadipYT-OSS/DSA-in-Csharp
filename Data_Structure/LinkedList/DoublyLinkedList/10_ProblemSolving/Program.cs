207fe5ffda853ab5d1ec66881982949b25b74873
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-02-23 23:10:57 +0530

    Updated file

---
+++ Data_Structure/LinkedList/DoublyLinkedList/10_ProblemSolving/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+    Triangle of user defined of integer sum in triangle pattern
+*/
+
+
+namespace triangleIntegerSumTrianglePattern {
+    class Solution() {
+        protected static void Sum(int indexVal) {
+            for (int i=1; i<=indexVal; i++) {
+                int sum = 0;
+                for (int j=1; j<=i; j++) {
+                    sum += j;
+                    if (j==i) {
+                        Console.Write(j);
+                    } else {
+                        Console.Write(j + "+");
+                    }
+                }
+                Console.WriteLine("=" + sum);
+            }
+        }
+
+        static void Main(string[] args) {
+            int n = 5;
+            Sum(n);
+        }
+    }
+}
