025e9a3704804a7b361f14d870b21a532adad871
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-20 11:53:47 +0530

    updated

--- Data_Structure/ArrayList/10_ArrayList_Methods/Program.cs
+++ Data_Structure/ArrayList/10_ArrayList_Methods/Program.cs
@@ -38,6 +38,30 @@ namespace ArrayList_Properties {
             al2.Add(9);
             Display(al2);
             Console.WriteLine("ArrayList capacity: " + al2.Capacity);
+
+            Console.WriteLine("\t - Example 3:");
+            ArrayList al3 = new ArrayList(10);  // Set capacity to 10
+            al3.Add(11);
+            al3.Add(12);
+            al3.Add(13);
+            al3.Add(14);
+            al3.Add(15);
+            al3.Add(16);
+            al3.Add(17);
+            al3.Add(18);
+            al3.Add(19);
+            al3.Add(20);
+            Display(al3);
+            Console.WriteLine("ArrayList capacity: " + al3.Capacity);
+
+            /*
+                Now you can see that in the 'al3' ArrayList, we have set the capacity to 10.
+                But if we want to add more than 10 elements, then the capacity will be automatically
+                increased. The new capacity will be double the current capacity, that is, 20.
+                Now if you do not want to increase the capacity doubled rather than you want to 
+                increase as per your new added elements, then you can use the TrimToSize() method.
+                In the example we will see how to use the TrimToSize() method.
+            */
         }
 
 
