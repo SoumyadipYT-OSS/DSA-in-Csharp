1cd11a1a0e8b2812f82aeed0121475737c371a50
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-23 00:54:55 +0530

    updated

--- Data_Structure/Struct/02_ParameterizedConstructor/Program.cs
+++ Data_Structure/Struct/02_ParameterizedConstructor/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ParameterizedConstructor_on_Csharp {
+    struct Student {
+        public int registrationNumber;
+        public string studentName;
+        public int rollNumber;
+
+        public Student(int regdNo, string name, int rollNo) {
+            this.registrationNumber = regdNo;
+            this.studentName = name;
+            this.rollNumber = rollNo;
+        }
+    }
+
+    public class Solution {
+        static void Main(string[] args) {
+            Student s1 = new Student(5896412, "Chulbul", 1);
+            Student s2 = new Student(8651233, "Sephali", 2);
+
+            Console.WriteLine("_Student 1_");
+            Console.WriteLine("Name: " + s1.studentName);
+            Console.WriteLine("Registration Number: " + s1.registrationNumber);
+            Console.WriteLine("Roll Number: " + s1.rollNumber);
+
+            Console.WriteLine("\n_Student 2_");
+            Console.WriteLine("Name: " + s2.studentName);
+            Console.WriteLine("Registration Number: " + s2.registrationNumber);
+            Console.WriteLine("Roll Number: " + s2.rollNumber);
+        }
+    }
+}
