67cb3ca96356dfb5b34725f8128e35686c153cda
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-03-23 14:45:11 +0530

    updated

---
+++ Data_Structure/Enum/01_AssignEnumValues/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AssignValues_to_Enum_Members {
+    enum Categories {
+        Electronics,
+        Food,
+        Automative = 6,
+        Arts,
+        BeautyCare,
+        Fashion
+    }
+
+
+    public class Solution {
+        static void Main(string[] args) {
+            Console.WriteLine("Electronics: " + (int)Categories.Electronics);
+            Console.WriteLine("Food: " + (int)Categories.Food);
+            Console.WriteLine("Automative: " + (int)Categories.Automative);
+            Console.WriteLine("Arts: " + (int)Categories.Arts);
+            Console.WriteLine("BeautyCare: " + (int)Categories.BeautyCare);
+            Console.WriteLine("Fashion: " + (int)Categories.Fashion);
+        }
+    }
+}
