2f78bd26a68f52575611297e49a649b68b50acea
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-04-24 17:03:06 +0530

    updated DynamicType

--- Data_Structure/DynamicType✅/pr4_MethodsParameters/Program.cs
+++ Data_Structure/DynamicType✅/pr4_MethodsParameters/Program.cs
@@ -7,12 +7,14 @@ and properties name of a dynamic type that holds the custom class object.
 namespace DYNAMIC_TYPE_METHODS_PARAMETERS {
     class Solution {
         static void Main(string[] args) {
+            dynamic stud = new Student();
 
+            // stud.DisplayStudentInfo("Soumyadip", 25);  This will throw runtime error because of wrong method name
         }
     }
 
     public class Student {
-        public void DisplayStudentInfo(string name, int age) {
+        public static void DisplayStudentInfo(string name, int age) {
             Console.WriteLine("Name: {0}, Age: {1}", name, age);
         }
     }
