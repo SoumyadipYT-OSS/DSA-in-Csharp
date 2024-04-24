5d33cdd36f913114020140ee7d2e4011ca7f997d
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-04-25 00:45:13 +0530

    updated

---
+++ Data_Structure/NullableType/pr3_HasValue/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NullableType_Methods {
+    class Solution {
+        static void Main(string[] args) {
+            Nullable<int> i = null;
+
+            if (i.HasValue)
+                Console.WriteLine(i.Value);
+            else
+                Console.WriteLine("Null");
+        }
+    }
+}
