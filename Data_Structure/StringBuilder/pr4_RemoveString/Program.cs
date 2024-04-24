862483962a5b9ead499953e1bbce25af54827e42
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-04-24 11:34:53 +0530

    Updated

---
+++ Data_Structure/StringBuilder/pr4_RemoveString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace REMOVESTRING {
+    class Solution {
+        static void RemoveStringMethod(StringBuilder sb, int index, int length) {
+            sb.Remove(index, length);
+            Console.WriteLine(sb);
+        }
+
+        static void Main(string[] args) {
+            StringBuilder sb = new StringBuilder("This is Visual Studio IDE for C#");
+            RemoveStringMethod(sb, 10, 8);    // Output: This is Studio IDE for C#
+        }
+    }
+}
