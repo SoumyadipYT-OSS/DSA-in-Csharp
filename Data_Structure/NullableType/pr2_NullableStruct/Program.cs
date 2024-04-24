a0460198c724176c695049dcfbef7685547a8d18
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-04-24 17:14:42 +0530

    updated code

---
+++ Data_Structure/NullableType/pr2_NullableStruct/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace NULLABLE_STRUCT {
+    [Serializable]
+    public struct Nullable<T> where T : struct {
+        public bool HasValue { get; }
+        public T value { get;  }
+
+        // other implementation
+    }
+
+    class Solution {
+        static void Main(string[] args) {
+            Nullable<int> i = null;
+
+            if (i.HasValue)
+                Console.WriteLine(i.value);
+            else
+                Console.WriteLine("Null");
+        }
+    }
+}
