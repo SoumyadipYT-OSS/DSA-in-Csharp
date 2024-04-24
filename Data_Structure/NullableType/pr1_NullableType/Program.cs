a13f1f5ceb14d4fcb944b0e4feada2454ce83703
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-04-24 17:11:45 +0530

    updated

---
+++ Data_Structure/NullableType/pr1_NullableType/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Text;
+
+namespace NULLABLE_TYPES {
+    class Solution {
+        static void Main(string[] args) {
+            Nullable<int> i = null;
+        }
+    }
+}
