d96969beb1fd2748d9be0767fa48a748c18e558b
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-04-25 01:13:28 +0530

    updated

---
+++ Data_Structure/NullableType/pr3_NullableMethods/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace NullableType_Methods {
+    class Solution {
+        static void GetValueOrDefault_Method() {
+            Nullable<int> i = null;
+            Console.WriteLine(i.GetValueOrDefault());
+        }
+
+        static void HasValue_Method() {
+            Nullable<int> i = null;
+
+            if (i.HasValue)
+                Console.WriteLine(i.Value);
+            else
+                Console.WriteLine("Null");
+        }
+
+        static void ShortHandSyntax() {
+            int? i = null;
+            double? d = null;
+            char? c = null;
+            bool? b = null;
+            Console.WriteLine(i);
+            Console.WriteLine(d);
+            Console.WriteLine(c);
+            Console.WriteLine(b);
+        }
+
+        static void OperatorMethod() {
+            int? i = null;
+            int j = i ?? 0;
+            Console.WriteLine(j);
+        }
+
+
+        static void Main(string[] args) {
+            GetValueOrDefault_Method();
+            HasValue_Method();
+            ShortHandSyntax();
+            OperatorMethod();
+        }
+    }
+}
---
+++ Data_Structure/NullableType/pr4_NullableType_Class/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NullableTypeClass {
+    class MyClass {
+        public Nullable<int> i = null;
+    }
+
+    class Solution {
+        static void Main(string[] args) {
+            MyClass mycls = new MyClass();
+
+            if (mycls.i == null)
+                Console.WriteLine("Null");
+            else
+                Console.WriteLine(mycls.i.Value);
+        }
+    }
+}
