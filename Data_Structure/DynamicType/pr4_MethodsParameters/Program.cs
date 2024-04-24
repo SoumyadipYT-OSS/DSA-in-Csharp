11eac27a24303b53eb9802301265066b61e6efea
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-04-24 16:11:42 +0530

    updated

---
+++ Data_Structure/DynamicType/pr4_MethodsParameters/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ If you assign a class object to the dynamic type, then the compiler would not check correct methods
+and properties name of a dynamic type that holds the custom class object.
+*/
+
+
+namespace DYNAMIC_TYPE_METHODS_PARAMETERS {
+    class Solution {
+        static void Main(string[] args) {
+
+        }
+    }
+
+    public class Student {
+        public void DisplayStudentInfo(string name, int age) {
+            Console.WriteLine("Name: {0}, Age: {1}", name, age);
+        }
+    }
+}
