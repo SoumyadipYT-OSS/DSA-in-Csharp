e6112ff91618088b8adb595797d414cea222face
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-05-18 14:28:18 +0530

    updated

---
+++ Data_Structure/ValueType_and_ReferenceType/ValueType/01_Ex1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ValueTypeExample1 
+{
+    class Solution {
+        protected static void ChangeValue(int x) {
+            x = 200;
+            Console.WriteLine(x);
+        }
+
+        // Main method
+        static void Main(string[] args) {
+            int i = 100;
+
+            Console.WriteLine(i);
+
+            ChangeValue(i);
+
+            Console.WriteLine(i);
+        }
+    }
+}
