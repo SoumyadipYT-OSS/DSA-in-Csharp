e364ef7e19278fd3b66a20e777ca03e86bfdc746
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-05-18 15:25:22 +0530

    updated code

---
+++ Data_Structure/ValueType_and_ReferenceType/ValueType/02_Ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Example2 
+{
+    class Solution {
+        protected static void ChangeValue(String str) {
+            str = "New Value";
+            Console.WriteLine(str);
+        }
+
+        // Main Method
+        static void Main(string[] args) {
+            String s = "Old Value";
+
+            Console.WriteLine(s);
+
+            ChangeValue(s);
+
+            Console.WriteLine(s);
+        }
+    }
+}
