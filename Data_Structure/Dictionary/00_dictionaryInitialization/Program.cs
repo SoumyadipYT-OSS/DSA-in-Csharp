85649f6fe0e5e1330de41584aae66a9b0bd70d9b
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-09-25 09:38:38 +0000

    updated

---
+++ Data_Structure/Dictionary/00_dictionaryInitialization/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System;
+
+class Dictionary00 {
+    static int Main(string[] args) {
+        Dictionary<int,string> dict = new Dictionary<int,string>();
+        Console.WriteLine(dict.ToString());
+        return 0;
+    }
+}
