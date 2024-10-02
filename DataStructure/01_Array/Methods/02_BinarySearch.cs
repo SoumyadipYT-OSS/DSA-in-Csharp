9296703d5fb68afcee02f94b0c778ff957237dee
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-10-02 14:49:52 +0530

    updated BinarySearch.cs

--- DataStructure/01_Array/Methods/02_BinarySearch.cs
+++ DataStructure/01_Array/Methods/02_BinarySearch.cs
@@ -1,5 +1,18 @@
 using System;
+using System.Collections.Generic;
 
-/*
-    
-*/
+class BinarySearchMethods {
+    protected static void BinarySearchExample1() {
+        int[] numbers = { 1, 3, 5, 7, 9, 11, 13 };
+        Arrary.Sort(numbers);
+
+        int valueToFind = 7;
+
+        // BinarySearch(Array, Object)
+        int index = Array.BinarySearch(numbers, valueToFind);
+
+        if (index >= 0) Console.WriteLine($"Value {valueToFind} found at index {index}.");
+        else Console.WriteLine($"Value {valueToFind} not found. Nearest index: {~index}.");
+
+    }
+}
