8d096678c654609826d14d813ca0e45d570ca48a
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-10-11 10:36:29 +0530

    updated 24_Sort.cs file

--- DataStructure/01_Array/Methods/24_Sort.cs
+++ DataStructure/01_Array/Methods/24_Sort.cs
@@ -14,4 +14,37 @@ class Program {
             return ( (new CaseInsensitiveComparer()).Compare(y, x) );
         }
     }
+
+
+    // Main method
+    static void Main(string[] args) {
+        // Creates and initializes a new Array and a new custom comparer.
+        String[] myKeys = { "red", "GREEN", "YELLOW", "BLUE", "purple", "black", "orange" };
+        String[] myValues = { "strawberries", "PEARS", "LIMES", "BERRIES", "grapes", "olives", "cantaloupe" };
+        IComparer myComparer = new myReverserClass();
+
+        // Displays the values of the Array.
+        Console.WriteLine("The Array initially contains the following values:");
+        PrintKeysAndValues(myKeys, myValues);
+
+        // Sorts a section of the Array using the default comparer.
+        Array.Sort(myKeys, myValues, 1, 3);
+        Console.WriteLine("After sorting a section of the Array using the default comparer:");
+        PrintKeysAndValues(myKeys, myValues);
+
+        // Sorts a section of the Array using the reverse case-insensitive comparer.
+        Array.Sort(myKeys, myValues, 1, 3, myComparer);
+        Console.WriteLine("After sorting a section of the Array using the reverse case-insensitive comparer:");
+        PrintKeysAndValues(myKeys, myValues);
+
+        // Sorts the entire Array using the default comparer.
+        Array.Sort(myKeys, myValues);
+        Console.WriteLine("After sorting the entire Array using the default comparer:");
+        PrintKeysAndValues(myKeys, myValues);
+
+        // Sorts the entire Array using the reverse case-insensitive comparer.
+        Array.Sort(myKeys, myValues, myComparer);
+        Console.WriteLine("After sorting the entire Array using the reverse case-insensitive comparer:");
+        PrintKeysAndValues(myKeys, myValues);
+    }
 }
