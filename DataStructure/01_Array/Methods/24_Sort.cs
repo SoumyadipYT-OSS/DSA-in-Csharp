eb7814d3a5c377e33c5b3d84991601228b6250a6
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-10-11 10:38:13 +0530

    updated methods

--- DataStructure/01_Array/Methods/24_Sort.cs
+++ DataStructure/01_Array/Methods/24_Sort.cs
@@ -2,14 +2,14 @@ using System;
 using System.Collections;
 
 class Program {
-    private static void PrintKeyAndValues(string[] myKeys, string[] myValues) {
+    private static void PrintKeysAndValues(string[] myKeys, string[] myValues) {
         for (int i=0; i<myKeys.Length; i++) {
             Console.WriteLine("\t {0 - 10}: {1}", myKeys[i], myValues[i]);
         }
         Console.WriteLine();
     }
 
-    protected class myReverserClass : Icomparer {
+    protected class myReverserClass : IComparer {
         int IComparer.Compare(object x, object y) {
             return ( (new CaseInsensitiveComparer()).Compare(y, x) );
         }
