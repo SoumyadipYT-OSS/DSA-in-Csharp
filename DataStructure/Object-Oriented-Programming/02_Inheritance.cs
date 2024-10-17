60852e62d68c2f902f51f9342e558994dbb92fe1
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-10-17 21:34:03 +0530

    Inheritance added

--- DataStructure/Object-Oriented-Programming/02_Inheritance.cs
+++ DataStructure/Object-Oriented-Programming/02_Inheritance.cs
@@ -1,15 +1,13 @@
 using System;
 
-
-private class Vehicle {
+public class Vehicle {
     public string Wheel { get; set; }
     public string Engine { get; set; }
 
-    public void Start() {
+    public virtual void Start() {
         Console.WriteLine("Vehicle started...");
     }
 }
-
 
 public class Car : Vehicle {
     public int NumberOfDoors { get; set; }
@@ -20,5 +18,31 @@ public class Car : Vehicle {
 
     public override void Start() {
         Console.WriteLine("Car started...");
+    }
+}
+
+public class Bike : Vehicle {
+    public int NumberOfDoors { get; set; }
+
+    public void Honk() {
+        Console.WriteLine("Bike honk!");
+    }
+
+    public override void Start() {
+        Console.WriteLine("Bike started...");
+    }
+}
+
+
+
+public class Concept_Inheritance {
+    static void Main() {
+        Car c = new Car();
+        c.Honk();
+        c.Start();
+
+        Bike b = new Bike();
+        b.Honk();
+        b.Start();
     }
 }
