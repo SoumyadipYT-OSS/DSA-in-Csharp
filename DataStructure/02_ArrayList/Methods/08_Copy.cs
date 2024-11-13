4351ba14ddead86b1fb91198d8c3c177cea3aae9
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-11-14 01:43:32 +0530

    ArrayList Copy Method is created

--- DataStructure/02_ArrayList/Methods/08_Copy.cs
+++ DataStructure/02_ArrayList/Methods/08_Copy.cs
@@ -1,67 +1,36 @@
+/*
+ 
+    Method: ArrayList.Copy
+    ArrayList to a one-dimensional array. Here's a simple C# 12 
+    program that demonstrates how to use the Copy method.
+ 
+*/
+
+
 using System;
 using System.Collections;
-public class SamplesArrayList {
 
-    public static void Main() {
+class Program {
+    static void Main() {
+        // Create a new ArrayList
+        ArrayList myList = new ArrayList();
 
-        // Creates and initializes the source ArrayList.
-        ArrayList mySourceList = new ArrayList();
-        mySourceList.Add("three");
-        mySourceList.Add("napping");
-        mySourceList.Add("cats");
-        mySourceList.Add("in");
-        mySourceList.Add("the");
-        mySourceList.Add("barn");
+        // Add some elements to the ArrayList
+        myList.Add(1);
+        myList.Add(2);
+        myList.Add(3);
+        myList.Add(4);
 
-        // Creates and initializes the one-dimensional target Array.
-        String[] myTargetArray = new String[15];
-        myTargetArray[0] = "The";
-        myTargetArray[1] = "quick";
-        myTargetArray[2] = "brown";
-        myTargetArray[3] = "fox";
-        myTargetArray[4] = "jumps";
-        myTargetArray[5] = "over";
-        myTargetArray[6] = "the";
-        myTargetArray[7] = "lazy";
-        myTargetArray[8] = "dog";
+        // Create an array to copy the elements into
+        int[] myArray = new int[myList.Count];
 
-        // Displays the values of the target Array.
-        Console.WriteLine("The target Array contains the following (before and after copying):");
-        PrintValues(myTargetArray, ' ');
+        // Copy elements from ArrayList to the array
+        myList.CopyTo(myArray);
 
-        // Copies the second element from the source ArrayList to the target Array starting at index 7.
-        mySourceList.CopyTo(1, myTargetArray, 7, 1);
-
-        // Displays the values of the target Array.
-        PrintValues(myTargetArray, ' ');
-
-        // Copies the entire source ArrayList to the target Array starting at index 6.
-        mySourceList.CopyTo(myTargetArray, 6);
-
-        // Displays the values of the target Array.
-        PrintValues(myTargetArray, ' ');
-
-        // Copies the entire source ArrayList to the target Array starting at index 0.
-        mySourceList.CopyTo(myTargetArray);
-
-        // Displays the values of the target Array.
-        PrintValues(myTargetArray, ' ');
-    }
-
-    public static void PrintValues(String[] myArr, char mySeparator) {
-        for (int i = 0; i < myArr.Length; i++)
-            Console.Write("{0}{1}", mySeparator, myArr[i]);
-        Console.WriteLine();
+        // Display elements of the array
+        Console.WriteLine("Elements in the array:");
+        foreach (var item in myArray) {
+            Console.WriteLine(item);
+        }
     }
 }
-
-
-/*
-This code produces the following output.
-
-The target Array contains the following (before and after copying):
- The quick brown fox jumps over the lazy dog
- The quick brown fox jumps over the napping dog
- The quick brown fox jumps over three napping cats in the barn
- three napping cats in the barn three napping cats in the barn
-*/
