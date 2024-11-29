d9699718aac8b1eae615e1fc00099180de41d15c
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-11-30 03:24:00 +0530

    Queue data structure is uploaded completed

---
+++ DataStructure/08_Queue✅/Methods/02_builtInMethods.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+
+
+
+class SampleProgram {
+    static void Main() {
+        // Creates and initializes a new Queue.
+        Queue myQ = new Queue();
+        myQ.Enqueue("The");
+        myQ.Enqueue("TATA");
+        myQ.Enqueue("Harrier");
+        myQ.Enqueue("SUV");
+
+        // Displays the Queue. [Enqueue]
+        Console.Write("Queue values: ");
+        PrintValues(myQ);
+
+        // Removes an element from the Queue. [Dequeue]
+        Console.WriteLine("(Dequeue)\t{0}", myQ.Dequeue());
+
+        // Displays the Queue.
+        Console.Write("Queue values:");
+        PrintValues(myQ);
+
+        // Views the first element in the Queue but does not remove it.
+        Console.WriteLine("(Peek)  \t{0}", myQ.Peek());
+
+        // Displays the Queue
+        Console.Write("Queue values: ");
+        PrintValues(myQ);
+
+        // Contains method
+        Console.Write("Does it Contain word \"SUV\"? " + myQ.Contains("SUV"));
+
+
+        // ToArray Method
+        object[] objArr = myQ.ToArray();
+        Console.WriteLine("Queue as array: \t" + string.Join(", ", objArr));
+    }
+
+
+    private static void PrintValues(IEnumerable myCollection) {
+        foreach (object obj in myCollection)
+            Console.Write(" \t {0}", obj);
+        Console.WriteLine();
+    }
+}
