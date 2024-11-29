8e369c10a685a93b827c9a5b725b72064b0f3dae
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-11-29 22:02:32 +0530

    string.Replace method is uploaded

--- DataStructure/06_Strings✅/Methods/06_Replace.cs
+++ DataStructure/06_Strings✅/Methods/06_Replace.cs
@@ -7,7 +7,7 @@ class Program {
     static void Main() {
         string str = "fatman";
         string replaced = str.Replace("f", "B");
-        Console.WriteLine(replaced);
+        Console.WriteLine(replaced);    // Batman
 
 
         string str2 = "cat";
@@ -15,18 +15,18 @@ class Program {
         replaced2 = replaced2.Replace("a", "A");
         replaced2 = replaced2.Replace("t", "T");
         Console.WriteLine(str2);
-        Console.WriteLine(replaced2);
+        Console.WriteLine(replaced2);   // RAT
 
 
         string str3 = "abc";
         string replaced3 = str3.Replace("b", " ");
         Console.WriteLine(str3);
-        Console.WriteLine(replaced3);
+        Console.WriteLine(replaced3);   // a c
 
 
         string str4 = "Deoo";
         string replaced4 = str4.Replace("o", "l");
         Console.WriteLine(str4);
-        Console.WriteLine(replaced4);
+        Console.WriteLine(replaced4);   // Dell
     }
 }
