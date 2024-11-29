6719ea6349122d33a8dbb877ac478d0cdb6ee2d3
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-11-29 23:10:15 +0530

    str.ToUpper() method is uploaded

---
+++ DataStructure/06_Strings✅/Methods/13_ToUpper.cs
@@ -0,0 +1,12 @@
+using System;
+
+/*  Converts the string to uppercase  */
+
+class Program {
+    static void Main() {
+        string str = "cloud";
+        string upper = str.ToUpper();
+
+        Console.WriteLine(upper);
+    }
+}
