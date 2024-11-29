0f5f6571aec79a8ab0f700890436b04a62435c2b
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-11-29 23:35:20 +0530

    str.IsNullOrEmpty() method is uploaded

---
+++ DataStructure/06_Strings✅/Methods/15_IsNullOrEmpty.cs
@@ -0,0 +1,10 @@
+using System;
+
+/*  IsNullOrEmpty: Checks if the string is null or empty.  */
+
+class Program {
+    static void Main() {
+        string str = "";
+        bool isNullOrEmpty = string.IsNullOrEmpty(str);     // true
+    }
+}
