8872bc4a48562bfba043a451e17d439929904747
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2025-01-13 00:21:54 +0530

    basicStructure is updated

---
+++ DataStructure/09_Tree✅/01_basicStructure.cs
@@ -0,0 +1,12 @@
+
+class TreeNode {
+    public int Value;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int Value) {
+        this.Value = Value;
+        left = null;
+        right = null;
+    }
+}
