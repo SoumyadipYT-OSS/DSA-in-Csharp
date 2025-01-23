7d86c31e0f27531255cf2bf0ede99f2c50077945
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2025-01-23 14:25:46 +0530

    Simple HashMap program is added

---
+++ DataStructure/10_HashTable_HashMap✅/HashMap/Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Example
+{
+    static void Main()
+    {
+        Dictionary<string, int> hashmap = new Dictionary<string, int>();
+
+        hashmap.Add("Lava", 1);
+        hashmap.Add("HCL", 2);
+        hashmap.Add("Micromax", 3);
+
+        Console.WriteLine("Value for 'Lava': " + hashmap["Lava"]);
+        Console.WriteLine("Value for 'HCL': " + hashmap["HCL"]);
+        Console.WriteLine("Value for Micromax: " + hashmap["Micromax"]);
+
+
+        if (hashmap.ContainsKey("date"))
+        {
+            Console.WriteLine("Value for 'date': " + hashmap["date"]);
+        } else
+        {
+            Console.WriteLine("'date' is not in the dictionary.");
+        }
+
+
+        hashmap.Remove("HCL");
+
+
+        Console.WriteLine("Updated dictionary:");
+        foreach (var kvp in hashmap)
+        {
+            Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+        }
+    }
+}
