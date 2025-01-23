8028a3cdde6934979637780660814e1f069df299
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2025-01-23 13:19:15 +0530

    Key property and sync root property these twos are added

---
+++ DataStructure/10_HashTable_HashMap/Hashtable/Properties✔️/SyncRootProperty.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+
+
+public class Example {
+	public static void Main() {
+		// Create a new Hashtable
+		Hashtable hashtable = new Hashtable();
+
+		// Add some elements to the Hashtable
+		hashtable.Add("apple", 1);
+		hashtable.Add("banana", 2);
+		hashtable.Add("cherry", 3);
+
+		// Display the keys in the Hashtable using the Keys property
+		Console.WriteLine("Keys in the Hashtable:");
+		foreach (var key in hashtable.Keys) {
+			Console.WriteLine(key);
+		}
+
+		// Add another element
+		hashtable.Add("date", 4);
+
+		// Display the updated keys in the Hashtable
+		Console.WriteLine("Updated keys in the Hashtable:");
+		foreach (var key in hashtable.Keys) {
+			Console.WriteLine(key);
+		}
+	}
+}
