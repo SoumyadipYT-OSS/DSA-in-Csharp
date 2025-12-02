b5c4419d8390fd7cce212717508d76defd75819e
SoumyadipYT-OSS <SoumyadipYT-OSS@users.noreply.github.com>
2025-12-02 07:05:09 +0530

    Properties of Dictionary is added

---
+++ DataStructure/17_Dictionary/Properties/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main()
+    {
+        var dict = new Dictionary<int, string>()
+        {
+            { 1, "One" },
+            { 2, "Two" },
+            { 3, "Three" }
+        };
+
+        // Capacity
+        Console.WriteLine($"Capacity: {dict.Capacity}");
+
+        // Count
+        Console.WriteLine($"Count: {dict.Count}");
+
+        // Comparer - EqualityComparer<TKey>.Default
+        Console.WriteLine($"Comparer: {dict.Comparer}");
+
+        // Keys
+        Console.WriteLine("Keys");
+        foreach (var k in dict.Keys)
+            Console.Write($" {k}");
+        Console.WriteLine();
+
+        // Item (Indexer) - access value by key
+        Console.WriteLine($"Item with key 2: {dict[2]}");
+
+        // Demonstrating update via indexer
+        dict[2] = "Two updated";
+        Console.WriteLine($"Updated item with key 2: {dict[2]}");
+
+        Console.ReadKey();
+    }
+}
