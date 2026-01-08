c10609f1a348ef317493a535a693f0c17cbf394c
Majumder, Soumyadip (Cognizant) <2416003@cognizant.com>
2026-01-08 06:18:31 +0530

    Merge Sort Divide and Conquer Approach

---
+++ Algorithms/_02_Divide_and_Conquer/_01_MergeSort/_01_MergeSort/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using static System.Console;
+
+namespace _01_MergeSort
+{
+    public class MergeSortAlgorithm
+    {
+        static void Main()
+        {
+            WriteLine("Merge Sort (Divide and Conquer approach)");
+            int[] arr = { 4, 2, 6, 9, 3 };
+            Algorithm.MergeSort(arr);
+            WriteLine(string.Join(", ", arr)); // 2, 3, 4, 6, 9
+        }
+    }
+}
