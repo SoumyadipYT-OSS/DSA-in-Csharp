d98c00724566a98bc19f424f776f38671484afab
Majumder, Soumyadip (Cognizant) <2416003@cognizant.com>
2026-01-08 16:03:23 +0530

    Find Grid Unique Path Memoization topic

---
+++ Algorithms/_03_Memoization/_03_GridUniquePath/_03_GridUniquePath/Algorithm.cs
@@ -0,0 +1,32 @@
+﻿
+namespace _03_GridUniquePath;
+
+public class Algorithm
+{
+    public static int FindGridUniquePath(int m, int n)
+    {
+        if (m <= 0 || n <= 0)
+            return 0;
+
+        int[,] memory = new int[m, n];
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                memory[i, j] = -1;
+
+        return GridUniquePathHelper(0, 0, m, n, memory);
+    }
+
+    private static int GridUniquePathHelper(int i, int j, int m, int n, int[,] memo)
+    {
+        if (i >= m || j >= n)
+            return 0;
+        else if (i == m - 1 && j == n - 1)
+            return 1;
+        else if (memo[i, j] != -1)
+            return memo[i, j];
+        else {
+            memo[i, j] = GridUniquePathHelper(i + 1, j, m, n, memo) + GridUniquePathHelper(i, j + 1, m, n, memo);
+            return memo[i, j];
+        }
+    }
+}
---
+++ Algorithms/_03_Memoization/_03_GridUniquePath/_03_GridUniquePath/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using static System.Console;
+
+namespace _03_GridUniquePath;
+
+public class Program
+{
+    static void Main()
+    {
+        WriteLine("Grid Unique Path problem");
+        int m = 3;
+        int n = 3;
+        WriteLine($"Column {m} x Row {n}");
+        WriteLine($"Output: {Algorithm.FindGridUniquePath(m, n)}");
+    }
+}
