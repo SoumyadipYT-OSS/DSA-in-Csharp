6020ce388d42a1ed8e17f1b1e4a577ce0e236ede
Majumder, Soumyadip (Cognizant) <2416003@cognizant.com>
2026-01-08 14:43:48 +0530

    Fibonacci Series Memoization cache memory implementation topic

---
+++ Algorithms/_03_Memoization/_01_FibonacciSeries/_01_FibonacciSeries/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using static System.Console;
+using System.Diagnostics;
+
+namespace _01_FibonacciSeries;
+
+public class Program
+{
+    static void Main()
+    {
+        int n = 40;
+
+        Stopwatch memoTimeStats = new();
+        memoTimeStats.Start();
+        long memoResult = Algorithm.FibonacciMemoization(n);
+        memoTimeStats.Stop();
+        long memoElapsedTime = memoTimeStats.ElapsedMilliseconds;
+        WriteLine($"Result: {memoResult}");
+        WriteLine($"Elapsed Time: {memoElapsedTime}");
+
+        Stopwatch naiveTimeStats = new();
+        naiveTimeStats.Start();
+        long naiveResult = Algorithm.FibonacciNaive(n);
+        naiveTimeStats.Stop();
+        long naiveElapsedTime = naiveTimeStats.ElapsedMilliseconds;
+        WriteLine($"Result: {naiveResult}");
+        WriteLine($"Elapsed Time: {naiveElapsedTime}");
+    }
+}
